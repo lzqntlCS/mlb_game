49f049f9db8ef71a1210d8cb1e9c2d7620ed2364
lzqntlCS <luiz.quintal@unesp.br>
2020-07-27 19:06:59 -0300

    Placar adicionado corretamente

--- mlbgame.py
+++ mlbgame.py
@@ -1,6 +1,7 @@
 #Simulador de jogo da MLB
 
 #Programar placar
+
 import random
 #starting score
 rs_score = [0,0,0,0,0,0,0,0,0]
@@ -24,4 +25,4 @@ y_tscore = sum(y_score)
 #New Score
 print(f'        1 2 3 4 5 6 7 8 9 R')
 print(f'REDSOX  {rs_score[0]} {rs_score[1]} {rs_score[2]} {rs_score[3]} {rs_score[4]} {rs_score[5]} {rs_score[6]} {rs_score[7]} {rs_score[8]} {rs_tscore}')
-print(f'YANKEES {y_score[0]} {y_score[1]} {y_score[2]} {y_score[3]} {y_score[4]}
+print(f'YANKEES {y_score[0]} {y_score[1]} {y_score[2]} {y_score[3]} {y_score[4]} {y_score[5]} {y_score[6]} {y_score[7]} {y_score[8]} {y_tscore}')
